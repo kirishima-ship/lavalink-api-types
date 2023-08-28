c41733609a3fe3e4e15e83f4ec7a658bf409f9d7
kagchi <yonuxbot@gmail.com>
2023-08-28 11:06:33 +0700

    chore: docs description

--- src/v3/routes/index.ts
+++ src/v3/routes/index.ts
@@ -1,47 +1,58 @@
 export const Routes = {
     /**
 	 * Route for:
-	 * - GET /loadtracks
+	 * - GET /v3/loadtracks
 	 */
     loadTracks(identifier: string) {
         return `/v3/loadtracks?identifier=${identifier}` as const;
     },
     /**
 	 * Route for:
-	 * - GET /decodetrack
+	 * - GET /v3/decodetrack
 	 */
     decodeTrack(track: string) {
         return `/v3/decodetrack?track=${track}` as const;
     },
     /**
 	 * Route for:
-	 * - POST /decodetracks
+	 * - POST /v3/decodetracks
 	 */
     decodeTracks() {
         return "/v3/decodetracks" as const;
     },
     /**
 	 * Route for:
-	 * - GET /routeplanner/status
+	 * - GET /v3/routeplanner/status
 	 */
     routePlannerStatus() {
         return "/v3/routeplanner/status" as const;
     },
     /**
 	 * Route for:
-	 * - POST /routeplanner/free/address
+	 * - POST /v3/routeplanner/free/address
 	 */
     routePlannerFreeAddress() {
         return "/v3/routeplanner/free/address" as const;
     },
     /**
 	 * Route for:
-	 * - POST /routeplanner/free/all
+	 * - POST /v3/routeplanner/free/all
 	 */
     routePlannerFreeAll() {
         return "/v3/routeplanner/free/all" as const;
     },
+    /**
+	 * Route for:
+	 * - WebSocket /v3/websocket
+	 */
+    websocket() {
+        return "/v3/websocket" as const;
+    },
 
+    /**
+	 * Route for:
+	 * - GET /version
+	 */
     version() {
         return "/version" as const;
     }
